190edabb32eb60887521053a90af5196b55eacf4
Ananya Singh <ananya1657@gmail.com>
2026-04-20 20:56:31 +0530

    toast

--- frontend/src/app/components/toast/toast.ts
+++ frontend/src/app/components/toast/toast.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToastService } from '../../services/toast';
 
 @Component({
   selector: 'app-toast',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './toast.html',
-  styleUrl: './toast.css',
+  styleUrl: './toast.css'
 })
-export class Toast {}
+export class ToastComponent {
+  constructor(public toastService: ToastService) {}
+}
